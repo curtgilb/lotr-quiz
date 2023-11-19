775e5844d948d4e7d2e908658186658aca624ab0
Curt Gilbert <me@curtgilbert.com>
2023-11-18 18:33:29 -0700

    Change to hash router

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { RouterProvider } from "react-router-dom";
-import { router } from "./routes";
+import { HashRouter } from "react-router-dom";
 import "./index.css";
 import "./styles/reset.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <HashRouter basename="/lotr-quiz" />
   </React.StrictMode>
 );
