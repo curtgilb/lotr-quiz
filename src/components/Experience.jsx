f7dff5e4834bce06437d97e5fa1074c4261c1991
Curt Gilbert <me@curtgilbert.com>
2024-12-16 15:29:35 -0700

    feature: ring glow on touch

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,5 +1,6 @@
 import { Environment } from "@react-three/drei";
 import { Ring } from "./Ring";
+import { Bloom, EffectComposer } from "@react-three/postprocessing";
 
 export function Experience() {
   return (
@@ -8,23 +9,24 @@ export function Experience() {
         files={[
           "./environment/1.png",
           "./environment/2.png",
+          "./environment/sauron_cropped.jpg",
           "./environment/3.png",
-          "./environment/4.png",
           "./environment/5.png",
           "./environment/6.png",
         ]}
-        intensity={0.05}
+        intensity={0.1}
       />
+      <ambientLight intensity={1.5} />
 
       <Ring />
 
-      {/* <EffectComposer>
+      <EffectComposer>
         <Bloom
-          intensity={4} // Adjust bloom intensity
-          luminanceThreshold={0.5} // Adjust what brightness level starts to glow
-          luminanceSmoothing={0.9} // Adjust how smooth the glow appears
+          intensity={1}
+          luminanceThreshold={0.5}
+          luminanceSmoothing={0.2}
         />
-      </EffectComposer> */}
+      </EffectComposer>
     </>
   );
 }
